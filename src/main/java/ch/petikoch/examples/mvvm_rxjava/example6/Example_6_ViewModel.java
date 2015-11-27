5b9a18b1219f239aea63db02916bba1ea210bb0e
peti <petikoch@gmail.com>
2015-11-27 11:03:05 +0100

    replaced german words by english words

--- src/main/java/ch/petikoch/examples/mvvm_rxjava/example6/Example_6_ViewModel.java
+++ src/main/java/ch/petikoch/examples/mvvm_rxjava/example6/Example_6_ViewModel.java
@@ -82,19 +82,19 @@ class Example_6_ViewModel implements IViewModel<Example_6_Model> {
             vm2v_resultat1.onNext("Creating acount...");
             vm2v_resultat2.onNext("Sending email...");
 
-            final Single<FinishedIndicator> erstelleKontoResultat = model.createAcount(nameFirstname);
-            erstelleKontoResultat.toObservable().first().takeUntil(v2vm_cancelButtonEvents).subscribe(
+            final Single<FinishedIndicator> createAccountResult = model.createAcount(nameFirstname);
+            createAccountResult.toObservable().first().takeUntil(v2vm_cancelButtonEvents).subscribe(
                     finishedIndicator -> vm2v_resultat1.onNext("Acount created for " + nameFirstname.getName() + " " + nameFirstname.getFirstname()));
-            v2vm_cancelButtonEvents.first().takeUntil(erstelleKontoResultat.toObservable()).subscribe(actionEvent -> vm2v_resultat1.onNext("Aborted"));
+            v2vm_cancelButtonEvents.first().takeUntil(createAccountResult.toObservable()).subscribe(actionEvent -> vm2v_resultat1.onNext("Aborted"));
 
-            final Single<FinishedIndicator> verschickeEmailResultat = model.sendEmail(nameFirstname);
-            verschickeEmailResultat.toObservable().first().takeUntil(v2vm_cancelButtonEvents).subscribe(
+            final Single<FinishedIndicator> sendEmailResult = model.sendEmail(nameFirstname);
+            sendEmailResult.toObservable().first().takeUntil(v2vm_cancelButtonEvents).subscribe(
                     finishedIndicator -> vm2v_resultat2.onNext("Email sent to " + nameFirstname.getName() + " " + nameFirstname.getFirstname()));
-            v2vm_cancelButtonEvents.first().takeUntil(verschickeEmailResultat.toObservable()).subscribe(actionEvent -> vm2v_resultat2.onNext("Aborted"));
+            v2vm_cancelButtonEvents.first().takeUntil(sendEmailResult.toObservable()).subscribe(actionEvent -> vm2v_resultat2.onNext("Aborted"));
 
             final Single<FinishedIndicator> beidesFertig =
-                    Single.zip(erstelleKontoResultat,
-                            verschickeEmailResultat,
+                    Single.zip(createAccountResult,
+                            sendEmailResult,
                             (finishedIndicator, finishedIndicator2) -> FinishedIndicator.INSTANCE
                     );
             Single.merge(beidesFertig, v2vm_cancelButtonEvents.first().toSingle())
